75b33b33955b2ceed6f1adf501091b5bb9d4846a
Bishal Bhattarai <bhattaraibishal50@gmail.com>
2020-09-17 18:28:52 +0545

    basic css and usages

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>That’s it! We now have the polished layout code in place to move onto our data fetching lessons.
+
+Before we wrap up this lesson, let’s talk about some helpful techniques related to Next.js’s CSS support on the next page.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
